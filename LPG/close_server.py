ce3a9ff2b1249d06593dc2b438a7dcb58257facf
kat-eul <katel.hgd@gmail.com>
2024-04-29 14:16:36 +0200

    feat : Adding a script closing the servers, and a desktop shortcut file.

---
+++ LPG/close_server.py
@@ -0,0 +1,27 @@
+import subprocess,os,sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+def separate_pid(server_pid):
+    all_server_pid = [""]
+    i = 0
+    for carac in server_pid :
+        if(carac == '\n'):
+            i+=1
+            all_server_pid.append("")
+        else :
+            all_server_pid[i]+=carac
+    return all_server_pid
+            
+cmd = ['/home/sandbox/.config/jupyterlab-desktop/jlab_server/bin/panel serve /home/sandbox/Documents/open_AR_Sandbox/LPG/server_sensor_calibration.py --show',
+       '/home/sandbox/.config/jupyterlab-desktop/jlab_server/bin/panel serve /home/sandbox/Documents/open_AR_Sandbox/LPG/server_projector_calibration.py --show',
+       '/home/sandbox/.config/jupyterlab-desktop/jlab_server/bin/panel serve /home/sandbox/Documents/open_AR_Sandbox/LPG/server_sandbox.py --show']
+
+for each in cmd :
+    ps_command = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
+    grep_command = subprocess.Popen(['grep', each], stdin=ps_command.stdout, stdout=subprocess.PIPE)
+    awk_command = subprocess.Popen(['awk', '{print $2}'], stdin=grep_command.stdout, stdout=subprocess.PIPE)
+    server_pid = separate_pid(awk_command.communicate()[0].decode().strip())
+
+    if len(server_pid)!=0:
+        for pid in server_pid :
+            os.kill(int(pid), 15)
---
+++ LPG/close_server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 /home/sandbox/Documents/open_AR_Sandbox/LPG/close_server.py

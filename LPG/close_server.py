140802b132287f6e66a77f2eb7550166b8851ef1
kat-eul <katel.hgd@gmail.com>
2024-04-30 09:05:03 +0200

    refactor : Instead of searching the entire command, only search panel serve that is commun with the three command. In this way, it doesn't need a modification by the user.

--- LPG/close_server.py
+++ LPG/close_server.py
@@ -12,20 +12,16 @@ def separate_pid(server_pid):
         else :
             all_server_pid[i]+=carac
     return all_server_pid
-            
-cmd = ['/home/sandbox/.config/jupyterlab-desktop/jlab_server/bin/panel serve /home/sandbox/Documents/open_AR_Sandbox/LPG/server_sensor_calibration.py --show',
-       '/home/sandbox/.config/jupyterlab-desktop/jlab_server/bin/panel serve /home/sandbox/Documents/open_AR_Sandbox/LPG/server_projector_calibration.py --show',
-       '/home/sandbox/.config/jupyterlab-desktop/jlab_server/bin/panel serve /home/sandbox/Documents/open_AR_Sandbox/LPG/server_sandbox.py --show']
 
-for each in cmd :
-    ps_command = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
-    grep_command = subprocess.Popen(['grep', each], stdin=ps_command.stdout, stdout=subprocess.PIPE)
-    awk_command = subprocess.Popen(['awk', '{print $2}'], stdin=grep_command.stdout, stdout=subprocess.PIPE)
-    server_pid = separate_pid(awk_command.communicate()[0].decode().strip())
 
-    if len(server_pid)!=0:
-        for pid in server_pid :
-            os.kill(int(pid), 15)
+ps_command = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
+grep_command = subprocess.Popen(['grep', 'panel serve'], stdin=ps_command.stdout, stdout=subprocess.PIPE)
+awk_command = subprocess.Popen(['awk', '{print $2}'], stdin=grep_command.stdout, stdout=subprocess.PIPE)
+server_pid = separate_pid(awk_command.communicate()[0].decode().strip())
+
+if len(server_pid)!=0:
+    for pid in server_pid :
+        os.kill(int(pid), 15)
 
 # Création de la fenêtre principale
 fenetre = Tk()
